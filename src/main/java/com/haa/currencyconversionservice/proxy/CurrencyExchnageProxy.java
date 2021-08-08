75046413400c62cf3ac44b81b252f5ae5d9cd303
ezazulhaq <61641801+ezazulhaq@users.noreply.github.com>
2021-08-08 17:35:05 +0530

    remove hardcoded URL to as client site load balancing is enabled on exchange and conversion service

--- src/main/java/com/haa/currencyconversionservice/proxy/CurrencyExchnageProxy.java
+++ src/main/java/com/haa/currencyconversionservice/proxy/CurrencyExchnageProxy.java
@@ -6,7 +6,8 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "currency-exchange-service", url = "localhost:8001")
+//@FeignClient(name = "currency-exchange-service", url = "localhost:8001")
+@FeignClient(name = "currency-exchange-service")
 public interface CurrencyExchnageProxy {
 
     @GetMapping(path = "/currency-exchange/from/{from}/to/{to}")
